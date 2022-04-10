0661d5f4a064d3446d30ad847f7b37299ef5f49c
voxasaurus <phil.vox@gmail.com>
2022-04-10 17:25:03 -0400

    codewars

---
+++ app2.js
@@ -0,0 +1,3 @@
+var number = 2022
+
+console.log(number.toString())
---
+++ app4.js
@@ -0,0 +1,5 @@
+function stringToArray(string){
+     stringarr = string.split(' ');
+	return stringarr;
+}
+stringToArray('The Quick Brown Fox Murders the Lazy Dog')
